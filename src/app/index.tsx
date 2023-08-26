235080cc27394d21fd7ea8e84e354794a600d71a
BobrikAU <kirbob0@yandex.ru>
2023-08-26 10:51:50 +0300

    initial: install dependencies, connect fonts

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ fontWeight: "400" }}>Таблица</h1>
+    </div>
+  );
+}
+
+export default App;
