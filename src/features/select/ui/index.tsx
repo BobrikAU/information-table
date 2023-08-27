ba22ae8ec02d365bf4ef184370e3404712131b2b
BobrikAU <kirbob0@yandex.ru>
2023-08-27 11:31:54 +0300

    features: make select

---
+++ src/features/select/ui/index.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import styles from "./index.module.scss";
+import { ArrowDown } from "../../../shared/icons";
+
+interface ISelectProps {
+  name: string;
+  description: string;
+  currentValue: string;
+  changeCurrentValue: React.Dispatch<React.SetStateAction<string>>;
+  possibleValues: string[];
+}
+
+const Select = ({
+  name,
+  description,
+  currentValue,
+  possibleValues,
+  changeCurrentValue,
+}: ISelectProps) => {
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+  // обработка выбора из выпадающего списка
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    changeCurrentValue(value);
+  };
+  // закрытие выпадающего спика при клике по значению выпадающего списка и текущему значению селекта
+  const handleClickLabel = () => {
+    setIsSelectOpen(false);
+  };
+  const handleClickCurrentValue = () => {
+    setIsSelectOpen(!isSelectOpen);
+  };
+  // формирование выпадающего списка
+  const selectList = possibleValues.map((value) => {
+    let option = (
+      <label
+        onClick={handleClickLabel}
+        className={styles.selectList__label}
+        key={nanoid()}
+      >
+        {value}
+        <input
+          name="singleChoice"
+          type="radio"
+          value={value}
+          checked={currentValue === value}
+          onChange={(e) => handleChange(e)}
+          className={styles.selectList__input}
+        />
+      </label>
+    );
+    return option;
+  });
+  // определение высоты выпадающего списка и передача его в css для нормальной работы анимации
+  // открытия и закрытия выпадающего списка
+  document.documentElement.style.setProperty(
+    "--selectListHeight",
+    `${28 * selectList.length}px`
+  );
+  return (
+    <div className={styles.blockSelect}>
+      <span className={styles.description}>{description}</span>
+      <form name={name}>
+        <div className={styles.select}>
+          <div
+            onClick={handleClickCurrentValue}
+            className={styles.currentValue}
+          >
+            <span className={styles.currentValue__span}>{currentValue}</span>
+            <ArrowDown
+              externalStyles={`${styles.currentValue__arrow} ${
+                isSelectOpen && styles.currentValue__arrow_rotated
+              }`}
+            />
+          </div>
+          <div
+            data-height={`${possibleValues.length * 28}px`}
+            className={`${styles.selectList} ${
+              isSelectOpen && styles.selectList_open
+            }`}
+          >
+            {selectList}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Select;
---
+++ src/shared/icons/index.ts
@@ -0,0 +1,3 @@
+import ArrowDown from "./arrowDown";
+
+export { ArrowDown };
